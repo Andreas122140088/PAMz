fad61ad1f171b527a3e0ac7f52a98f89ce52672d
Muhammad Fauzi Azizi <145269330+Fauzi122140106@users.noreply.github.com>
2025-05-18 13:36:42 +0700

    j

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,6 +104,13 @@ dependencies {
     implementation (libs.androidx.material.icons.extended)
     implementation (libs.androidx.datastore.preferences)
     implementation("io.coil-kt:coil-compose:2.5.0")
+    implementation ("androidx.compose:compose-bom:2024.10.00")
+    implementation ("androidx.compose.material3:material3")
+    implementation ("androidx.compose.ui:ui")
+    implementation ("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation ("androidx.compose.ui:ui-tooling")
+    implementation ("androidx.activity:activity-compose:1.9.3")
+
 }
 
 // Pass options to Room ksp processor
